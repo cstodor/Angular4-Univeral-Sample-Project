da7e97fac4109d029fd884a84ab18f3a54bcf5d5
Csaba <todorcs@gmail.com>
2017-07-20 19:18:32 +0300

    Set Up Routing fro Home and Contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//Components
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: HomeComponent
+  },
+    {
+    path: 'contact',
+    component: ContactComponent
   }
 ];
 
