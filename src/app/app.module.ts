8ad289ed4266a72a78662fdbbd08e27dc9839548
Csaba <todorcs@gmail.com>
2017-07-20 19:17:55 +0300

    Generated Home Component
    + to generate new components use the --module=<module-name.ts> flag to
    avoid errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Modules
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'angular-universal'}),
