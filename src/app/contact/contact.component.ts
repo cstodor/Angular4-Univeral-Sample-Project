2ed585ebdfdba6be1cf7fccb0bf5b6246b0c7502
Csaba <todorcs@gmail.com>
2017-07-20 19:34:13 +0300

    Addd Title & Meta Tags to Home and Contact
    + imported Meta & Title from angular/platform-browser, injected them
    into the constructor function and set Title and Tags for both
    Components.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-contact',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    meta: Meta,
+    title: Title
+  ) {
+    title.setTitle('Contact Us');
+    meta.addTags([
+      { name: 'author', content: 'Csaba Todor' },
+      { name: 'keywords', content: 'contact, angular, angular universal, node.js, express.js, typescript, javascript' },
+      { name: 'description', content: 'Contact Page for Angular 4 Sample Project Using Angular Universal' }
+    ]);
+  }
 
   ngOnInit() {
   }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    meta: Meta,
+    title: Title
+  ) {
+    title.setTitle('Angular Universal Example Project');
+    meta.addTags([
+      { name: 'author', content: 'Csaba Todor' },
+      { name: 'keywords', content: 'angular, angular universal, node.js, express.js, typescript, javascript' },
+      { name: 'description', content: 'Angular 4 Sample Project Using Angular Universal' }
+    ]);
+  }
 
   ngOnInit() {
   }
