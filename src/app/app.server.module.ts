637ec4bec0a3fb6bf4dfe4695d00b05d88ecb888
Csaba <todorcs@gmail.com>
2017-07-20 22:10:27 +0300

    Fixed Reference Error
    FIXED "ReferenceError: document is not defined at
    DefaultDomRenderer2.selectRootElement" Error

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,13 +1,32 @@
-import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { NgModule, NgModuleFactory, NgModuleFactoryLoader, RendererFactory2, NgZone } from '@angular/core';
+import { ServerModule, ɵServerRendererFactory2 } from '@angular/platform-server';
+import { NoopAnimationsModule, ɵAnimationRendererFactory } from '@angular/platform-browser/animations';
+import { ɵAnimationEngine } from '@angular/animations/browser';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
+// declarations
+export function instantiateServerRendererFactory(
+    renderer: RendererFactory2, engine: ɵAnimationEngine, zone: NgZone) {
+    return new ɵAnimationRendererFactory(renderer, engine, zone);
+}
+
+export const SERVER_RENDER_PROVIDERS = [{
+    provide: RendererFactory2,
+    useFactory: instantiateServerRendererFactory,
+    deps: [ɵServerRendererFactory2, ɵAnimationEngine, NgZone]
+}
+];
+
 @NgModule({
-imports: [
-    ServerModule,
-    AppModule
-],
-bootstrap: [AppComponent]
+    imports: [
+        ServerModule,
+        NoopAnimationsModule,
+        AppModule
+    ],
+    bootstrap: [AppComponent],
+    providers: [
+        SERVER_RENDER_PROVIDERS
+    ]
 })
 export class AppServerModule { }
